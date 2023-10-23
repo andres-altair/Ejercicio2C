dd53267618c948800f5369351de1f92474787727
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-10-23 14:31:41 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,25 @@
+﻿using Ejercicio2C;
+using Ejercicio2C.Servicios;
+
+namespace Ejercicio2C
+{
+    class program
+    {
+        /// <summary>
+        /// metodo por donde entra la aplicacion
+        /// 231023-aamd
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            pedirI pI = new pedirC();
+            int a = pI.pedir();
+            if (a == )
+            {
+                Console.WriteLine("se puede expresar como el cuadrado de otro número entero ");
+            }
+            else { Console.WriteLine("no se puede expresar como el cuadrado de otro número entero"); }
+        }
+    }
+}  
+
---
+++ Servicios/pedirC.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2C.Servicios
+{
+    internal class pedirC : pedirI
+    {
+        public int pedir()
+        {
+            Console.WriteLine("escribe un numero entero");
+            int a = Convert.ToInt32(Console.ReadLine());
+            return a;
+        }
+}
